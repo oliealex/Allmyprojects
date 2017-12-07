4c380729fd4f4a4586eb4b5312acc78f09a61556
oliealex <alexander_racho@hotmail.com>
2017-12-07 21:54:07 +0100

    JAvascript button stuff

--- testcookieclicker/script.js
+++ testcookieclicker/script.js
@@ -1,4 +1,15 @@
-function onClick()
-{
-  
+
+
+//document.getElementById('navbarMagic').onclick = function(){myFunction(i)};
+
+function sleep(delay) {
+    var start = new Date().getTime();
+    while (new Date().getTime() < start + delay);
+}
+
+
+function myFunction(){
+      document.getElementsByClassName('test')[0].style.background = "black";
+      document.getElementsByClassName('test')[0].style.transform = "scale(5%, 50px);";
+      document.getElementsByClassName('test')[0].style.transition = "5s";
 }
